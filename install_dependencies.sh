5dfd5556c278ce2a508298a31143538fd76a4276
jackh <jackhessburg94@gmail.com>
2020-05-01 22:45:39 -0600

    updated install dep

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#if [[ "$OSTYPE" == "linux-gnu" ]]; then
-#  brew install pyqt5
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install pyqt5
 
-#elif [[ "$OSTYPE" == "darwin"* ]]; then
-#  sudo apt install python3-pyqt5
+if [[ "$OSTYPE" == "linux-gnu"]]; then
+  sudo apt install python3-pyqt5
 
-pip3 install affine pillow opencv-python rasterio torch torchvision shapely sklearn utm pyproj PyQt5 matplotlib pandas
+sudo pip3 install affine pillow opencv-python rasterio torch torchvision shapely sklearn utm pyproj matplotlib pandas
