b87cb7717e71ba79977a3e093c42f9f34a87f391
Pai-Sho <jackhessburg94@gmail.com>
2020-05-01 22:21:37 -0600

    added install dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#  brew install pyqt5
+
+#elif [[ "$OSTYPE" == "darwin"* ]]; then
+#  sudo apt install python3-pyqt5
+
+pip3 install affine pillow opencv-python rasterio torch torchvision shapely sklearn utm pyproj PyQt5 matplotlib pandas
