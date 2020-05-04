613276c47c2a89daa2924c270103000e633bdd77
Herzfeld-Lab <64673710+Herzfeld-Lab@users.noreply.github.com>
2020-05-04 13:39:57 -0600

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  brew install pyqt5
+  pip3 install pyqt5
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt install python3-pyqt5
 else
   echo "System not recognized"
 fi
 
-sudo pip3 install affine pillow opencv-python rasterio torch torchvision shapely sklearn utm pyproj matplotlib pandas
+sudo pip3 install affine pillow opencv-python rasterio torch torchvision shapely sklearn utm pyproj matplotlib pandas pyyaml
