2cd0398cf6d578efa9fc3bdd8296b7e3fc802bd8
Adam Hayes <adha9240@colorado.edu>
2021-07-22 11:39:09 -0600

    made performance test script for dda class more functional

--- test_dda_class_perf.py
+++ test_dda_class_perf.py
@@ -1,19 +1,65 @@
 
 import numpy as np
-import os
+import os, glob
 
 
-def compare_labels(fileTrain, fileValid, eCheck):
+def compare_labels(fileTrain, fileValid, num_classes = 4):
 
 	train_labels = np.load(fileTrain, allow_pickle=True)[1]
 	train_labels = train_labels[:,-1]
 
-	valid_labels = np.load(fileValid, allow_pickle=True)[1]
-	valid_labels = valid_labels[:,4]
+	for fp in fileValid:
 
-	bools = train_labels==valid_labels
-	num_correct = bools[bools==True].shape[0]
+		epoch = fp.split('/')[-1]
+		print('Epoch checkpoint: ', epoch)
 
+		valid_labels = np.load(fp, allow_pickle=True)[1]
+		valid_labels = valid_labels[:,4]
+
+		bools = train_labels==valid_labels
+		num_correct = bools[bools==True].shape[0]
+
+		print('Total Correct: {} out of {}'.format(num_correct,len(train_labels)))
+
+		for i in range(num_classes):
+
+			idxs = train_labels == i
+			tot = idxs[idxs==True].shape[0]
+			class_bools = train_labels[idxs] == valid_labels[idxs]
+			nc = class_bools[class_bools==True].shape[0]
+			pc = nc / tot
+
+			print('Total in Class {}: {} Total correct: {} Percent correct: {}'.format(i, tot, nc, pc))
+
+		print('\n')
+
+def main():
+
+	base_labels = '/Users/adamhayes/workspace/NN_Class/Config/dda_test_negri_valid/dda_test_negri_valid_still_testing.npy'
+	valid_label_dir = '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels'
+	valid_data = glob.glob(valid_label_dir + '/*.npy')
+	valid_paths = []
+	for file in valid_data:
+		path_full = valid_label_dir + file
+		valid_paths.append(path_full)
+
+	compare_labels(base_labels,valid_paths)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+"""
 	idxs0 = train_labels == 0
 	idxs1 = train_labels == 1
 	idxs2 = train_labels == 2
@@ -47,21 +93,4 @@ def compare_labels(fileTrain, fileValid, eCheck):
 	print("Total in Class 2: ", tot2, " Total correct: ", num2correct, " Percent correct: ", perc2correct)
 	print("Total in Class 3: ", tot3, " Total correct: ", num3correct, " Percent correct: ", perc3correct)
 	print("\n")
-
-
-def main():
-
-	base_labels = '/Users/adamhayes/workspace/NN_Class/Config/dda_test_negri_valid/dda_test_negri_valid_still_testing.npy'
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_20.npy',20)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_41.npy',41)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_61.npy',61)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_82.npy',82)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_100.npy',100)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_115.npy',115)
-	compare_labels(base_labels, '/Users/adamhayes/workspace/NN_Class/Output/dda_test_negri_21-07-2021_16:14/labels/labeled_epoch_128.npy',128)
-
-
-if __name__ == '__main__':
-	main()
-
-
+"""
