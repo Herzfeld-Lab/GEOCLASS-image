27bb61cf969043af28a01ec390392d6aadf74e64
Adam Hayes <adha9240@colorado.edu>
2022-05-18 13:22:41 -0600

    integrating parallelized vario computation

--- varioAlongTrack.py
+++ varioAlongTrack.py
@@ -7,16 +7,18 @@ import itertools
 import haversine as hs
 from haversine import Unit
 from datetime import datetime
+from multiprocessing import Pool
+from functools import partial
 
 def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False, residual = False):
 
-	# TODO: clean up unnecessary code
+	# TODO: add few more comments
 
 	###########################
 	## PARAMETER DEFINITIONS ##
 	###########################
 	# :ddaData: --> path to dda produced ground_estimate or weighted_photon dataset
-	# :lag: --> distances between pairs of points at which variograms are computed
+	# :lag: --> # resolution of the variogram (i.e. the spacing of the lags)
 	# :windowSize: --> size of alog-track data chunk (in meters) to compute variograms on
 	# :windowStep: --> magnitude of which to shift window (in meters) at every iteration of variogram computation
 	# :ndir: --> number of directions to compute variograms (default = 1)
@@ -25,9 +27,11 @@ def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False,
 	# :residual: --> Bool to determine whether to run residual variograms or base variograms
 	###########################
 
-	lag = float(lag) # resolution of the variogram (i.e. the spacing of the lags)
-	# nres = int(windowSize / lag) # Number of results to calculate (depends on window size and lag size)
-	###########################
+	lag = float(lag)
+	
+	# smoothing coefficients
+	coef = np.array([.0625,0.25,0.375,0.25,0.625])
+	coef = coef/coef.sum() # normalize
 
 	# Load the output data from the surface detector code, likely in the following format
 	# [bin_lon, bin_lat, bin_elev, bin_distance, bin_elev_stdev, bin_density_mean, bin_weighted_stdev]
@@ -41,7 +45,6 @@ def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False,
 	if photons==True:
 		# Format of photon data:
 		# [delta_time, longitude, latitude, elevation, distance]
-		# delta_time = track_data[:,0]
 		lon = track_data[:,1]
 		lat = track_data[:,2]
 		distance = track_data[:,4] # distance along track in meters
@@ -51,8 +54,6 @@ def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False,
 		lat = track_data[:,1]
 		distance = track_data[:,3] # distance along track in meters
 		elevation = track_data[:,2] # corresponding interpolated elevation
-		# delta_time = track_data[:,4]
-		# density = track_data[:,6]
 
 	# Calculate eastings and northings based on lon-lat data
 	eastings, northings, _, _ = utm.from_latlon(lat,lon)
@@ -63,19 +64,13 @@ def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False,
 	windows = np.array(windows)
 
 	# initialize return array
-	vario_values_ret = np.zeros((len(windows),nres))
-
-	# PARALLELIZATION:
-	# use forloop to divide up data into list of window_data objects
-	# call pool.starmap() on this iterable list
-	# move compute_varios() inside this function (to inherit parameters)
-	# compute_varios() should still only take in a window_data segment (not whole thing)
+	data_windows_all = []
 
 	for w in range(0,len(windows)):
 		# Subset the elevation data
 		start = windows[w,0]
 		end = windows[w,1]
-		
+
 		window_bool = np.logical_and(distance>=start,distance<end)
 		window_data = np.array([eastings[window_bool],northings[window_bool],elevation[window_bool]]).T
 		# window_data = np.array([lat[window_bool],lon[window_bool],elevation[window_bool]]).T
@@ -83,28 +78,17 @@ def run_vario(ddaData, lag, windowSize, windowStep, ndir, nres, photons = False,
 			print('too few points in current window')
 			continue
 
-		vario_results = compute_varios(window_data, ndir, lag, nres)
+		data_windows_all.append(window_data)
 
-		if len(vario_results) != nres:
-			print('Less than nres # of vario values computed: {}'.format(len(vario_results)))
 
-		if len(vario_results)<5:  # if there were fewer than 5 variogram values calculated
-			print('too few vario results to use')
-			continue
-
-		# SMOOTHING of variogram values with linear filter
-		coef = np.array([.0625,0.25,0.375,0.25,0.625])
-		coef = coef/coef.sum() # normalize
-		vario_values = convolve1d(vario_results, coef, mode='nearest')
-
-		vario_values_ret[w] = vario_values
+	with Pool() as pool:
+		# vario_values_ret = pool.map(compute_varios, data_windows_all)
+		vario_values_ret = pool.map(partial(compute_varios, lag=lag, nres=nres, coef=coef), data_windows_all)
 
 	return np.array(vario_values_ret)
 
 
-
-def compute_varios(windowData, ndir, lag, nres):
-
+def compute_varios(windowData, lag, nres, coef):
 	"""
 	window data: [east (utm), north (utm), elevation]
 	"""
@@ -127,14 +111,7 @@ def compute_varios(windowData, ndir, lag, nres):
 		varSum /= (2 * len(pairs))
 		return varSum
 
-	def haversine_dist(arr1, arr2):
-		# print(arr1)
-		return hs.haversine(arr1,arr2, unit=Unit.METERS)
-
-
-	##########################################################
-	################  functionality begins  ##################
-	##########################################################
+	##### Functionality begins #####
 	# get all pairwise distances between points in the window
 	pdist = pairwise_distances(windowData[:,0:2])
 	vario_values = []
@@ -145,8 +122,7 @@ def compute_varios(windowData, ndir, lag, nres):
 			vario_values.append(0.0)
 			continue
 		vario_values.append(semivariogram(pairs))
-
-	return vario_values
-
-
 	
+	vario_values = convolve1d(vario_values, coef, mode='nearest')
+
+	return np.array(vario_values)
