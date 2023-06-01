a0e2d2d74aaf91bc808bf151406f50ffbf7913ae
rajo7273 <rajo7273@colorado.edu>
2023-06-01 17:32:39 -0600

    removed sudo requirement

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sudo pip3 install pyqt5
+  pip3 install pyqt5
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt install python3-pyqt5
 else
   echo "System not recognized - Please install on MacOS or Ubuntu Linux"
 fi
 
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
